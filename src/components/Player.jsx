3578e181a307b01d7b4505abeb8c1960353793d6
Tyler Geerdts <tygee713@gmail.com>
2022-11-30 15:57:43 -0500

    Updates to player movement

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,40 +2,49 @@ import React from 'react'
 import { useSphere } from '@react-three/cannon'
 import { useFrame, useThree } from '@react-three/fiber'
 import { useEffect, useRef } from 'react'
-import { Vector3 } from 'three'
+import { Raycaster, Vector3 } from 'three'
 import { useKeyboard } from '../hooks/useKeyboard'
 
 const JUMP_FORCE = 5
-const SPEED = 8
+const SPEED = 300
 
 export const Player = () => {
   const { moveBackward, moveForward, moveLeft, moveRight, jump } = useKeyboard()
-  const { camera } = useThree()
+  const { camera, scene } = useThree()
 
   // Player object
-  const [ref, api] = useSphere(() => ({
-    mass: 1,
-    type: 'Dynamic',
+  const [playerRef, player] = useSphere(() => ({
+    mass: 100,
+    fixedRotation: true,
     position: [0, 1, 0],
+    args: 0.2,
+    material: {
+      friction: 0
+    }
   }))
 
   // The velocity of the player object
   // This acts as an instance variable that will persist between re-renders
   // Gets set to the velocity of the sphere object according to outside forces
-  const vel = useRef([0, 0, 0])
 
-  useEffect(() => api.velocity.subscribe((v) => vel.current = v), [api.velocity])
+  const state = useRef({
+    vel: [0, 0, 0],
+    pos: [0, 0, 0],
+    jumping: false
+  })
+  const { vel, pos } = state.current
 
+  useEffect(() => player.velocity.subscribe((v) => vel = v), [player.velocity])
+s
   // The position of the player object, for use by the camera
   // This acts as an instance variable that will persist between re-renders
   // Gets set to the position of the sphere object after outside forces are applied
-  const pos = useRef([0, 0, 0])
 
-  useEffect(() => api.position.subscribe((p) => pos.current = p), [api.position])
+  useEffect(() => player.position.subscribe((p) => pos = p), [player.position])
 
   // This runs every frame, so 60 times per second
   useFrame(() => {
-    camera.position.copy(new Vector3(pos.current[0], pos.current[1], pos.current[2]))
+    camera.position.copy(new Vector3(pos[0], pos[1], pos[2]))
 
     const direction = new Vector3()
     const frontVector = new Vector3(0, 0, (moveBackward ? 1 : 0) - (moveForward ? 1 : 0))
@@ -47,12 +56,27 @@ export const Player = () => {
       .multiplyScalar(SPEED)
       .applyEuler(camera.rotation)
 
-    api.velocity.set(direction.x, vel.current[1], direction.z)
+    player.velocity.set(direction.x, vel[1], direction.z)
 
-    if (jump && Math.abs(vel.current[1] < 0.05)) {
-      api.velocity.set(vel.current[0], JUMP_FORCE, vel.current[2])
+    if (state.current.jumping && vel[1] < 0) {
+      /** Ground check */
+      const raycaster = new Raycaster(
+        player.position,
+        new Vector3(0, -1, 0),
+        0,
+        0.2
+      );
+      const intersects = raycaster.intersectObjects(scene.children);
+      if (intersects.length !== 0) {
+        state.current.jumping = false
+      }
+    }
+
+    if (Math.abs(vel[1]) < 0.05 && !state.current.jumping && jump) {
+      player.velocity.set(vel[0], JUMP_FORCE, vel[2])
+      state.current.jumping = true
     }
   })
 
-  return <mesh ref={ref}></mesh>
+  return <mesh ref={playerRef}></mesh>
 }
